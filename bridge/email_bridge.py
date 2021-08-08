7fd0dcd11909c9df84b597f7f20658c942e7dab1
4sf15cs122@sahyadri.edu.in <4sf15cs122@sahyadri.edu.in>
2021-08-08 16:58:35 +0530

    handled all the exceptions rectified all the errors for windows

--- bridge/email_bridge.py
+++ bridge/email_bridge.py
@@ -5,11 +5,31 @@ import smtplib
 import sys
 import os
 import json
-print(os.system("pwd"))
+import re
+
+regex = '^[a-z0-9]+[\._]?[ a-z0-9]+[@]\w+[. ]\w{2,3}$'
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class InvalidEmail(Error):
+    """Invalid Email Error"""
+    pass
+
+
+def check(email):
+    if re.search(regex, email):
+        return True
+    else:
+        return False
+
 
 try:
     print("inside try")
-    loc = os.getcwd()+"/files/email.json"
+    loc = os.getcwd() + "/files/email.json"
 
     print("**location**")
     print(loc)
@@ -28,20 +48,29 @@ try:
     FROM = emails["from"]
     PASSWORD = emails["password"]
     TO = data["to"]
+    if not check(TO):
+        raise InvalidEmail
     SUBJECT = data["subject"]
     messages = data["message"]
     message = MIMEMultipart()
     message['From'] = FROM
-    message['To'] =TO
+    message['To'] = TO
     message['Subject'] = SUBJECT
     # The body and the attachments for the mail
     message.attach(MIMEText(messages, 'plain'))
     print("setting up server")
-    session = smtplib.SMTP(SERVER, PORT)
+    session = smtplib.SMTP_SSL(SERVER, PORT)
+    session.ehlo()
     session.starttls()  # enable security
+    session.ehlo()
+    print("login into mail")
     session.login(FROM, PASSWORD)  # login with mail_id and password
     text = message.as_string()
+    print("sending mail")
     session.sendmail(FROM, TO, text)
+    print("mail sent")
     session.quit()
 except json.JSONDecodeError:
     print("Failed to Execute")
+except InvalidEmail:
+    print("Invalid Email Error")
